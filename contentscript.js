5d08732aa38beb9349d5fdb13f409db028ce1f4b
Irakli Safareli <i.safareli@gmail.com>
2013-01-06 01:17:49 +0400

    added ::selection

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,19 @@
 $('*:not(img,.syntaxhighlighter)').css({
-	'background': '#000000',
+	'background-color': 'transparent',//#000000
 	'color':"#878A85",
-	"border-color": '#A03'
+	"border-color": 'rgba(168, 78, 105, 0.5)'
 });
 
 $('a').css('color',"#906");
 $('h1,h2,h3,h4,h5,h6').css('color','#DD1515');
 $('code').css({
-	'background': '#1B2426',
+	'background-color': '#1B2426',
 	'color':"#E9C228"
 });
 $('body').css('background','url('+ chrome.extension.getURL('debut_dark.png')+') repeat 0 0');
+
+var ss = document.styleSheets[0];
+var selectedCss = "selection {background:transparent; color:#E08524;}";
+ss.insertRule('::-moz-'+selectedCss, 0);
+ss.insertRule('::'+selectedCss, 0);
+ss.insertRule('::-webkit-'+selectedCss, 0);
