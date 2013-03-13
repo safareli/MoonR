67c801e292f1e6de33f9f209ac4e0da0fd1116c0
Irakli Safareli <i.safareli@gmail.com>
2013-03-13 23:41:01 +0400

    v2.0
    removed jquery and usees css

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,9 @@
-$('*:not(.syntaxhighlighter)').css({
-	'background-color': 'transparent',//#000000
-	'color':"#878A85",
-	"border-color": 'rgba(168, 78, 105, 0.5)'
-});
-
-$('a').css('color',"#906");
-$('h1,h2,h3,h4,h5,h6').css('color','#DD1515');
-$('code').css({
-	'background-color': '#1B2426',
-	'color':"#E9C228"
-});
-$('body').css('background','url('+ chrome.extension.getURL('debut_dark.png')+') repeat 0 0');
+MoonR = {
+	html:document.getElementsByTagName("html")[0],
+	switcher : function () {
+		if (this.html.classList.contains('moonr-injected'))
+			this.html.classList.remove('moonr-injected');
+		else
+			this.html.classList.add('moonr-injected');
+	}
+};
--- eventPage.js
+++ eventPage.js
@@ -1,9 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(tab.tabId, { file: 'jquery.min.js' }, function() {
-    chrome.tabs.executeScript(tab.tabId, { file: 'contentscript.js' }, function() {
-      chrome.tabs.insertCSS(tab.tabId, { file: 'contentstyle.css' }, function() {
-        console.log('injected');
-      });
-    });
-  });
+	chrome.tabs.executeScript(tab.tabId, { code: 'MoonR.switcher();' });
 });
